c5f8d66174f3c9577dfe3a8d8b2d079f1ed2eef2
wow <110139562+yty-yang@users.noreply.github.com>
2023-07-11 21:25:56 +0800

    finish basic issues

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,150 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable, V extends Comparable> implements Map61B<K, V> {
+    private Node node;
+    private int size;
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left;
+        private Node right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        boolean containsKey(K key) {
+            if (this.key == null) {
+                return false;
+            }
+
+            if (key.equals(this.key)) {
+                return true;
+            } else if (key.compareTo(this.key) < 0) {
+                return left.containsKey(key);
+            } else {
+                return right.containsKey(key);
+            }
+        }
+
+        V get(K key) {
+            if (this.key == null) {
+                return null;
+            }
+
+            if (key.equals(this.key)) {
+                return this.value;
+            } else if (key.compareTo(this.key) < 0) {
+                return left.get(key);
+            } else {
+                return right.get(key);
+            }
+        }
+
+        void put(K key, V value) {
+            if (this.key == null) {
+                this.key = key;
+                this.value = value;
+                this.left = new Node(null, null);
+                this.right = new Node(null, null);
+                size++;
+            } else if (key.equals(this.key)) {
+                this.value = value;
+            } else if (key.compareTo(this.key) < 0) {
+                this.left.put(key, value);
+            } else {
+                this.right.put(key, value);
+            }
+        }
+    }
+
+    public BSTMap() {
+        node = new Node(null, null);
+    }
+
+    @Override
+    public void clear() {
+        node = null;
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (node == null) {
+            return false;
+        }
+
+        return node.containsKey(key);
+    }
+
+    @Override
+    public V get(K key) {
+        if (node == null) {
+            return null;
+        }
+
+        return node.get(key);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        node.put(key, value);
+    }
+
+    @Override
+    public Set keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new iterator();
+    }
+
+    private class iterator implements Iterator<K> {
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public K next() {
+            return null;
+        }
+    }
+
+    public void printInOrder() {
+        for (K i: this) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String Args[]) {
+        BSTMap<Integer, Integer> m = new BSTMap<>();
+        System.out.println(m.size());
+        for (int i = 0; i < 20; i++) {
+            m.put(i, i);
+            System.out.println(m.size());
+        }
+    }
+}
